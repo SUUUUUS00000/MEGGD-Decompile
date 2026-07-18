a70a97a43dc9c096743d48ed47bb691f5351b788
SUUUUUS00000 <specialakk69@gmail.com>
2026-07-18 12:27:52 +0300

    Refactor bytecode decompilation to use Module

--- src/decompiler.cpp
+++ src/decompiler.cpp
@@ -1,28 +1,19 @@
 // decompiler.cpp
 #include "decompiler.hpp"
-
-#include "bytecode_reader.hpp"
 #include "structurizer.hpp"
 #include "code_printer.hpp"
-
-#include <vector>
 
 namespace luaudec
 {
 
-std::string decompile_bytecode(const uint8_t* data, size_t size)
+std::string decompileModule(const Module& module)
 {
-    Module m = BytecodeReader::read(data, size);
+    std::vector<std::vector<StmtPtr>> bodiesByProto(module.protos.size());
+    for (const Proto& proto : module.protos)
+        bodiesByProto[proto.selfIndex] = structureFunction(module, proto);
 
-    std::vector<std::vector<StmtPtr>> bodies;
-    bodies.reserve(m.protos.size());
-    for (const auto& proto : m.protos)
-    {
-        bodies.push_back(structureFunction(m, proto));
-    }
-
-    CodePrinter printer(m, bodies);
-    return printer.printChunk(m.mainProtoId);
+    CodePrinter printer(module, bodiesByProto);
+    return printer.printChunk(module.mainProtoId);
 }
 
 } // namespace luaudec
